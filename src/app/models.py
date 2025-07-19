0fe553e03d0528335d384357d1121f37cc97c90e
Maxim Proskurin <Mx.prskn@yandex.ru>
2025-07-19 21:32:21 +0300

    модели

---
+++ src/app/models.py
@@ -0,0 +1,24 @@
+import uuid
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.dialects.postgresql import UUID
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        unique=True,
+        nullable=False
+    )
+    username = Column(
+        String(30),
+        unique=True,
+        index=True,
+        nullable=False
+    )
+    hashed_password = Column(
+        String,
+        nullable=False
+    )
