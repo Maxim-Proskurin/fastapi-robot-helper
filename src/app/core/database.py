ee834ac790ed96d8a699f0ca8a8590b76391a5fa
Maxim Proskurin <Mx.prskn@yandex.ru>
2025-07-22 18:30:31 +0300

    linters

--- src/app/core/database.py
+++ src/app/core/database.py
@@ -1,10 +1,6 @@
 from typing import AsyncGenerator
 
-from sqlalchemy.ext.asyncio import (
-    AsyncSession,
-    async_sessionmaker,
-    create_async_engine
-)
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import declarative_base
 
 from src.app.core.config import settings
